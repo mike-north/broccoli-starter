5f441e390fd875277f8a11bf5592fcbf8cbc5830
Glitch (silver-swift) <none>
2018-07-19 17:23:35 +0000

    🍞🙄 Checkpoint ./src/-index.html:413660/5 ./package.json:413660/269 ./Brocfile.js:413660/1293 ./src/app.js:413660/106 ./src/index.js:413660/17

--- Brocfile.js
+++ Brocfile.js
@@ -1,44 +1,47 @@
 var Funnel = require('broccoli-funnel');
 var Concat = require('broccoli-concat');
 var MergeTrees = require('broccoli-merge-trees');
+var Babel = require('broccoli-babel-transpiler');
+const BroccoliLivereload = require('broccoli-livereload');
 
 // root of our source files
 var projectFiles = 'src';
 
-/* get a new node of only files in the 'src/css' directory
-  cssFiles contains the following files:
-
-  ├── reset.css
-  └── todos.css
-*/
+/* get a new node of only '*.css' files in the 'src' directory */
 var cssFiles = new Funnel(projectFiles, {
   include: ['**/*.css']
 });
 
-/* get a new node of only files in the 'src/css' directory
-  cssFiles contains the following files:
-
-  ├── reset.css
-  └── todos.css
-*/
+/* get a new node of only *.js files in the 'src' directory */
 var jsFiles = new Funnel(projectFiles, {
   include: ['**/*.js']
 });
 
-/* get a new node of only files in the 'src/css' directory
-  cssFiles contains the following files:
-
-  ├── reset.css
-  └── todos.css
-*/
+/* get a new node of only *.html files in the 'src' directory */
 var htmlFiles = new Funnel(projectFiles, {
   include: ['**/*.html']
 });
 
 
+const reloadable = new BroccoliLivereload(projectFiles, {
+    target: 'index.html'
+})
+
 
 module.exports = new MergeTrees([
   cssFiles,
-  new Concat(jsFiles, { outputFile: 'out.js' }),
-  htmlFiles
+  new Concat(
+    new Babel(jsFiles, {
+      browserPolyfill: true,
+      presets: [
+        ['env', {
+          'targets': {
+            'browsers': ['last 1 versions']
+          }
+        }]
+      ]
+    }),
+    { outputFile: 'out.js' }
+  ),
+  reloadable
 ]);
